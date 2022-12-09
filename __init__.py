fba5a80bd0bf07625d76e964d4219072a0b14784
SIGPwny <sigpwny@gmail.com>
2022-12-09 19:30:12 +0000

    match github repo naming

--- __init__.py
+++ __init__.py
@@ -30,20 +30,20 @@ class CryptohackChallenge(BaseChallenge):
     name = "cryptohack"  # Name of a challenge type
 
     templates = {  # Handlebars templates used for each aspect of challenge editing & viewing
-        "create": "/plugins/cryptohack_challenges/assets/create.html",
-        "update": "/plugins/cryptohack_challenges/assets/update.html",
-        "view": "/plugins/cryptohack_challenges/assets/view.html",
+        "create": "/plugins/ctfd-cryptohack-plugin/assets/create.html",
+        "update": "/plugins/ctfd-cryptohack-plugin/assets/update.html",
+        "view": "/plugins/ctfd-cryptohack-plugin/assets/view.html",
     }
     scripts = {  # Scripts that are loaded when a template is loaded
-        "create": "/plugins/cryptohack_challenges/assets/create.js",
-        "update": "/plugins/cryptohack_challenges/assets/update.js",
-        "view": "/plugins/cryptohack_challenges/assets/view.js",
+        "create": "/plugins/ctfd-cryptohack-plugin/assets/create.js",
+        "update": "/plugins/ctfd-cryptohack-plugin/assets/update.js",
+        "view": "/plugins/ctfd-cryptohack-plugin/assets/view.js",
     }
     # Route at which files are accessible. This must be registered using register_plugin_assets_directory()
-    route = "/plugins/cryptohack_challenges/assets/"
+    route = "/plugins/ctfd-cryptohack-plugin/assets/"
     # Blueprint used to access the static_folder directory.
     blueprint = Blueprint(
-        "cryptohack_challenges",
+        "ctfd-cryptohack-plugin",
         __name__,
         template_folder="templates",
         static_folder="assets",
@@ -186,9 +186,9 @@ class CryptohackChallenge(BaseChallenge):
 
 
 def load(app):
-    upgrade(plugin_name="cryptohack_challenges")
+    upgrade(plugin_name="ctfd-cryptohack-plugin")
     app.db.create_all()
     CHALLENGE_CLASSES["cryptohack"] = CryptohackChallenge
     register_plugin_assets_directory(
-        app, base_path="/plugins/cryptohack_challenges/assets/"
+        app, base_path="/plugins/ctfd-cryptohack-plugin/assets/"
     )
